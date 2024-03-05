f8d1a0abea99afa2e6060e10db5b79ffdaff52f3
eapldev <eduardoaparecido.dev@gmail.com>
2024-03-04 22:03:33 -0300

    Criação e estilização de Header/Footer.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
+import Header from './components/Header'
+import Footer from './components/Footer'
+
 function App() {
   return (
     <>
+    <Header />
       <h1> Hello World! Olá Mundo!</h1>
       <p> Olá Mundo, estou aprendendo Reackt.Js</p>
+      <Footer />
     </>
   );
 }
---
+++ src/components/Footer/index.js
@@ -0,0 +1,11 @@
+import styles from './Footer.module.css'
+
+function Footer() {
+    return(
+        <footer className={styles.footer}>
+            <h2>CineFlix &copy; Desenvolvido por Eapl 2024</h2>
+        </footer>
+    )
+}
+
+export default Footer
