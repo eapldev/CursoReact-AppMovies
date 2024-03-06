b88b93539ad2dbfbd630dadcfc47efc428d20dd5
eapldev <eduardoaparecido.dev@gmail.com>
2024-03-06 19:18:57 -0300

    Atribuindo componentes com props e children

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import Header from './components/Header'
 import Footer from './components/Footer'
+import Banner from './components/Banner';
+import Container from './components/Container';
 
 function App() {
   return (
     <>
     <Header />
+    <Banner  image="home"/>
+    <Container>
       <h1> Hello World! Olá Mundo!</h1>
       <p> Olá Mundo, estou aprendendo Reackt.Js</p>
+    </Container>
       <Footer />
     </>
   );
