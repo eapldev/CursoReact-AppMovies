951a8310f7a83bef0bfc1f46046306bf091be3d9
eapldev <eduardoaparecido.dev@gmail.com>
2024-03-07 20:26:34 -0300

    Criando Rotas e Páginas, React Router DOM

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,12 +1,15 @@
+import { Link } from "react-router-dom";
 import style from "./Header.module.css"
 
 function Header() {
     return (
         <header className={style.header}>
-            <span>CineFlix</span>
+            <Link to="/">
+                <span>CineFlix</span>
+            </Link>
             <nav>
-                <a href="">Home</a>
-                <a href="">Assistir</a>
+                <Link to="/">Home</Link>
+                <Link to="/watch">Assistir</Link>
             </nav>
         </header>
     );
