344b3bbe455fe2aa2e8fc742ec4c8d81da5e3db8
eapldev <eduardoaparecido.dev@gmail.com>
2024-03-06 22:43:38 -0300

    Atribuindo componentes de categoria e filtrar videos

--- src/App.js
+++ src/App.js
@@ -3,7 +3,20 @@ import Footer from './components/Footer'
 import Banner from './components/Banner';
 import Container from './components/Container';
 import Card from './components/Card';
-import videos from './json/db.json'
+import videos from './json/videos.json'
+import Category from './components/Category';
+
+const categories = [
+  "Geografia",
+  "Como fazer e usar",
+  "Astronomia e Geografia",
+  "Climatologia, Meteorologia, Vegetação",
+  "Geologia e Hidrografia",
+]
+
+function filterCategory(id) {
+  return videos.filter( video => video.category === categories[id] )
+}
 
 function App() {
   return (
@@ -11,10 +24,22 @@ function App() {
       <Header />
       <Banner image="home" />
       <Container>
-        <h2>Counter Strike 2 - BEST PLAYERS</h2>
-        <section className="cards">
-          { videos.map((video) => <Card id={video.id} key={video.id} /> )}
-        </section>
+        <Category category={categories[0]}>
+        { filterCategory(0).map((video) => <Card id={video.id} key={video.id} /> )}
+        </Category>
+        <Category category={categories[1]}>
+        { filterCategory(1).map((video) => <Card id={video.id} key={video.id} /> )}
+        </Category>
+        <Category category={categories[2]}>
+        { filterCategory(2).map((video) => <Card id={video.id} key={video.id} /> )}
+        </Category>
+        <Category category={categories[3]}>
+        { filterCategory(3).map((video) => <Card id={video.id} key={video.id} /> )}
+        </Category>
+        <Category category={categories[4]}>
+        { filterCategory(4).map((video) => <Card id={video.id} key={video.id} /> )}
+        </Category>
+
       </Container>
       <Footer />
     </>
