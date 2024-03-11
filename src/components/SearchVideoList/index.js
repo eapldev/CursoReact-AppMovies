38216483172c34b28736b260e49fb34f98032c2d
eapldev <eduardoaparecido.dev@gmail.com>
2024-03-11 20:16:52 -0300

    Criando Componente SearchVideoList, Pesquisar vídeos por categoria ou título

---
+++ src/components/SearchVideoList/index.js
@@ -0,0 +1,31 @@
+import styles from './SearchVideoList.module.css'
+import VideoList from '../VideoList'
+import { useState } from 'react'
+
+
+function filterVideos(videos, searchText) {
+    return videos.filter((video) => video.category.includes(searchText) || video.title.includes(searchText))
+}
+
+function SearchVideoList( { videos }) {
+
+    const [ searchText, setSearchText ] = useState()
+    const foundVideos = filterVideos(videos, searchText)
+
+    return(
+        <section className={styles.container}>
+            <input
+              type="search"
+              placeholder="Pesquisar..."
+              value={searchText}
+              onChange={e => setSearchText(e.target.value)}
+            />
+            <VideoList
+               videos={foundVideos}
+               emptyHeading={`Sem videos sobre "${searchText}"`}
+            />
+        </section>
+    )
+}
+
+export default SearchVideoList
