5fda0d9ce067f974eb77dac265b9594be6e2d17b
eapldev <eduardoaparecido.dev@gmail.com>
2024-03-14 21:57:10 -0300

    Adicionando Componente Form/Página para cadastrar vídeos + rota para acessar a página

---
+++ src/pages/VideoCadastre/index.js
@@ -0,0 +1,17 @@
+import Header from '../../components/Header'
+import Container from '../../components/Container'
+import Form from '../../components/Form'
+import Footer from '../../components/Footer'
+
+function VideoCadastre() {
+    return(
+        <>
+           <Header />
+           <Container>
+              <Form />
+           </Container>
+           <Footer />
+        </>
+    )
+}
+export default VideoCadastre
