182856ce05055772dbb2a4155eb96d9b6a47f441
eapldev <eduardoaparecido.dev@gmail.com>
2024-03-14 21:20:23 -0300

    Atribuindo Context,FavoriteContext. Criação de Hook Personalizado para controlar estado do contexto + ajustes nas rotas e nos cards

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,22 +1,33 @@
 import { Link } from "react-router-dom"
 import styles from "./Card.module.css"
-import iconFavorite from "./favorite.png"
-import iconUnFavorite from "./unfavorite.png"
-
+import iconFavorite from "./favorite.png"     /* branco */
+import iconUnfavorite from "./unfavorite.png" /* vermelho */
+import { useFavoriteContext } from "../../contexts/Favorites"
 
 function Card({ id }) {
-    return(
-        <section className={styles.card}>
-            <Link to={`/watch/${id}`}>
-                <img src={`https://img.youtube.com/vi/${id}/mqdefault.jpg`}
-                 alt="Capa" className={styles.cover}/>
-            </Link>
-            <figure className={styles.icon}>
-                <img src={iconFavorite} alt="Ícone" />
 
-            </figure>
+    const { favorite, addFavorite } = useFavoriteContext()
+    const isFavorite = favorite.some((fav) => fav.id === id)
+	const icone = !isFavorite ? iconFavorite : iconUnfavorite
+
+    return (
+        <section className={styles.card}>
+            <Link to={`/watch/${id}`} >
+                <img
+                    src={`https://img.youtube.com/vi/${id}/mqdefault.jpg`}
+                    alt="Capa"
+                    className={styles.capa}
+                />
+                </Link>
+                <figure className={styles.icon}>
+                    <img
+                        src={icone}
+                        alt="Ícone"
+                        onClick={() => addFavorite({id})}
+                    />
+                </figure>
         </section>
-    )
+    );
 }
 
 export default Card
